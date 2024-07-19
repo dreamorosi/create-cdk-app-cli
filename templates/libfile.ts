e42009bed4e64e034c1ef26aeb9e92c587250b67
Andrea Amorosi <dreamorosi@gmail.com>
2024-07-19 11:27:31 +0200

    fix: path & tracing

--- templates/libfile.ts
+++ templates/libfile.ts
@@ -1,22 +1,34 @@
-import { Stack, type StackProps, CfnOutput } from 'aws-cdk-lib';
+import { Stack, type StackProps, CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Runtime, Tracing } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction, OutputFormat } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
 
 export class pascalcase-nameStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const fnName = 'pascalcase-nameFn';
+    const logGroup = new LogGroup(this, 'MyLogGroup', {
+      logGroupName: `/aws/lambda/${fnName}`,
+      removalPolicy: RemovalPolicy.DESTROY,
+      retention: RetentionDays.ONE_DAY,
+    });
     const fn = new NodejsFunction(this, 'MyFunction', {
+      functionName: fnName,
+      logGroup,
       runtime: Runtime.NODEJS_20_X,
-      entry: '../src/index.ts',
+      entry: './src/index.ts',
       handler: 'handler',
+      tracing: Tracing.ACTIVE,
       bundling: {
         minify: true,
         mainFields: ['module', 'main'],
         sourceMap: true,
         format: OutputFormat.ESM,
-      }
+      },
+      banner: 
+          "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
     });
 
     new CfnOutput(this, 'FunctionArn', {
