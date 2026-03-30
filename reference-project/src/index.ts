f95847b339e68e46bd034af932169df06e5e5356
Andrea Amorosi <dreamorosi@gmail.com>
2026-03-30 17:49:47 +0800

    chore: removed unused files

--- reference-project/src/index.ts
+++
@@ -1,8 +0,0 @@
-import type { Context } from 'aws-lambda';
-
-export const handler = async (_event: unknown, context: Context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify('Hello, World!'),
-  };
-};
